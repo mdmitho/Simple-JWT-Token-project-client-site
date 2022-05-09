44ae34ab4542b1d0ead47c98856b0908580fa996
Hossen <mdmitho199911@gmail.com>
2022-05-09 18:50:40 +0600

    Send JWT Token to the server side and set middletier

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -5,28 +5,22 @@ const Orders = () => {
     const navigate = useNavigate();
     const [orders, setOrders] = useState([]);
 
-    // useEffect( () =>{
-    //     fetch('http://localhost:5000/orders', {
-    //         method: 'GET', 
-    //         headers:{
-    //             authorization: `Bearer ${localStorage.getItem('accessToken')}`
-    //         }
-    //     })
-    //     .then(res => {
-    //         if(res.status === 401 || res.status===403){
-    //             navigate('/login');
-    //         }
-    //         return res.json()
-    //     })
-    //     .then(data =>{
-    //         console.log(data);
-    //         setOrders(data);
-    //     })
-    // }, [])
+    useEffect(()=>{
+        fetch('http://localhost:5000/orders',{
+            method:'GET',
+            headers:{
+                authorization:`Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+  .then(res=>res.json())
+  .then(data =>{
+      console.log(data);
+  })
+    },[])
 
     return (
         <div>
-            <h1>This is Orders page: {orders.length}</h1>
+            {/* <h1>This is Orders page: {orders.length}</h1> */}
         </div>
     );
 };
